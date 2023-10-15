2f9ffd39ef36f21b8ee4830e8a0ed1c31f4c43db
Saihan Sadique <saihan.sadique1@gmail.com>
2023-10-15 09:30:27 +0530

    more changes

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
+
 function getAction(){
     return Action = Math.floor(Math.random()*3);
 }
-let computerChoice = getAction();
-console.log(computerChoice);
+let playerAction = prompt("Enter your action (rock|paper|scissors)")
+let computerAction = getAction();
+function playRound(computerAction,playerAction){
+    if(computerAction == 0){
+
+    }
+    else if(computerAction == 1){
+
+    }
+    else if(computerAction == 2){
+        
+    }
+}
