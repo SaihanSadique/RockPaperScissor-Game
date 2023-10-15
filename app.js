4b9276d7904dad0f3f70f64e3e444eb7511c8317
Saihan Sadique <saihan.sadique1@gmail.com>
2023-10-15 18:20:22 +0530

    structured code to follow logic

--- app.js
+++ app.js
@@ -1,35 +1,65 @@
 
-function getAction(){
-    return Action = Math.floor(Math.random()*3);
+function getAction() {
+    return Action = Math.floor(Math.random() * 3);
 }
-let playerAction = prompt("Enter your action (rock|paper|scissors)")
-let computerAction = getAction();
-function playRound(computerAction,playerAction){
-    if(computerAction == 0){
+
+function playRound(computerAction, playerAction) {
+    let playerAction = prompt("Enter your action (rock|paper|scissors)").toUpperCase();
+    let computerAction = getAction();
+    if (computerAction == 0) {
         computerAction = "ROCK";
     }
-    else if(computerAction == 1){
+    else if (computerAction == 1) {
         computerAction = "PAPER";
     }
-    else if(computerAction == 2){
+    else if (computerAction == 2) {
         computerAction = "SCISSORS";
     }
-    playerAction = playerAction.toUpperCase();
-    switch (playerAction)
-    {
+    switch (playerAction) {
         case 'ROCK':
-            if(computerAction == "ROCK") return prompt("It's a tie!");
-            if(computerAction == "PAPER") return prompt("you lose!");
-            if(computerAction == "SCISSORS") return prompt("you win!");
+            if (computerAction == "ROCK") {
+                return prompt("It's a tie!");
+            }
+            if (computerAction == "PAPER") {
+                ComputerWin += 1;
+                return prompt("you lose!");
+            }
+            if (computerAction == "SCISSORS") {
+                playerWin += 1;
+                return prompt("you win!");
+            }
         case 'PAPER':
-            if(computerAction == "ROCK") return prompt("you win!");
-            if(computerAction == "PAPER") return prompt("it's a tie!");
-            if(computerAction == "SCISSORS") return prompt("you lose!");
+            if (computerAction == "ROCK"){
+                playerWin+=1;
+                return prompt("you win!");
+            }
+            if (computerAction == "PAPER") {
+                return prompt("it's a tie!");
+            }
+            if (computerAction == "SCISSORS") {
+                ComputerWin+=1;
+                return prompt("you lose!");
+            }
         case 'SCISSORS':
-            if(computerAction == "ROCK") return prompt("you lose!");
-            if(computerAction == "PAPER") return prompt("you win!");
-            if(computerAction == "SCISSORS") return prompt("it's a tie!");
-        default: prompt("invalid input, please try again!")
-
+            if (computerAction == "ROCK") {
+                ComputerWin+=1;
+                return prompt("you lose!");
+            }
+            if (computerAction == "PAPER") {
+                playerWin+=1;
+                return prompt("you win!");
+            }
+            if (computerAction == "SCISSORS") {
+                return prompt("it's a tie!");
+            }
+        default: alert("invalid input, please try again!")
     }
 }
+let playerWin = 0, ComputerWin = 0; //First one to reach 5 wins, wins the BO5
+while (playerWin < 5 || ComputerWin < 5) {
+    playRound();
+}
+if (playerWin == 5)
+    alert("Player wins the Best of five!")
+if (ComputerWin == 5)
+    alert("Computer wins the Best of five!")
